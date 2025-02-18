4c1ac2fdf0511416909dcc7c279b2cf3f6b90f39
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-18 03:02:44 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,7 +2,7 @@ from os import path, getenv
 
 class Config:
     API_ID = int(getenv("API_ID", "26043952"))
-    API_HASH = getenv("API_HASH", "96b8dea447ef580b5b75b01ccc3ab710)
+    API_HASH = getenv("API_HASH", "96b8dea447ef580b5b75b01ccc3ab710")
     BOT_TOKEN = getenv("BOT_TOKEN", "7633669044:AAHiS9PWSZkyZeHdTA6m8-6PYNf9DKitfvQ")
     FSUB = getenv("FSUB", "AMBOTYT")
     CHID = int(getenv("CHID", "-1000112234"))
