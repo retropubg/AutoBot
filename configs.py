1758f3d9708e672431db2bc20851f716cc8e7dbf
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-18 03:29:06 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,16 +1,16 @@
-from os import path, getenv
+import os
 
 class Config:
-    API_ID = int(getenv("API_ID", "26043952"))
-    API_HASH = getenv("API_HASH", "96b8dea447ef580b5b75b01ccc3ab710")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7633669044:AAHiS9PWSZkyZeHdTA6m8-6PYNf9DKitfvQ")
-    FSUB = getenv("FSUB", "AMBOTYT")
-    CHID = int(getenv("CHID", "-1000112234"))
-    
-sudo_env = os.getenv("SUDO", "").strip()
-SUDO = list(map(int, sudo_env.split())) if sudo_env else []
+    API_ID = int(os.getenv("API_ID", "26043952"))
+    API_HASH = os.getenv("API_HASH", "96b8dea447ef580b5b75b01ccc3ab710")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7633669044:AAHiS9PWSZkyZeHdTA6m8-6PYNf9DKitfvQ")
+    FSUB = os.getenv("FSUB", "AMBOTYT")
+    CHID = int(os.getenv("CHID", "-1000112234"))
 
+    # Convertir la variable de entorno SUDO en una lista de enteros
     SUDO = list(map(int, os.getenv("SUDO", "").split())) if os.getenv("SUDO") else []
+
+    # URI de MongoDB
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:XTKWKCzhppgfmcSoenGawpAdLxDdKAOE@mongodb.railway.internal:27017")
 
 cfg = Config()
